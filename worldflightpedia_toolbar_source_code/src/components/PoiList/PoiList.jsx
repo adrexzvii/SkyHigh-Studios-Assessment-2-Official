f2733934e0de080bb7be43234fe7312e5fc6e9ff
Adrian Zegarra <adrianzv199830@gmail.com>
2025-11-15 12:16:08 -0400

    refactor(poiList): removed unused imports

--- worldflightpedia_toolbar_source_code/src/components/PoiList/PoiList.jsx
+++ worldflightpedia_toolbar_source_code/src/components/PoiList/PoiList.jsx
@@ -18,7 +18,6 @@ import React from "react";
 import { List, Typography, Box } from "@mui/material";
 import palette from "../../theme/palette";
 import PoiListItem from "./PoiListItem";
-import PoiListEmpty from "./PoiListEmpty";
 import { usePoiSelection } from "../../hooks/map/usePoiSelection";
 import { usePoiContext } from "../context/PoiContext";
 
